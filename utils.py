59c0f285f5b045b5b025d96eab7998360b0e3dc3
eltrompetero <edl56@cornell.edu>
2018-04-25 18:17:01 -0400

    Removed some unnecessary code.

--- utils.py
+++ utils.py
@@ -96,16 +96,6 @@ def add_to_pickle(v,fname):
     out.close()
     return
 
-def dump_pickle(mdict,fname,use_pickle=False):
-    """
-    2017-02-12
-    """
-    if usepickle:
-        import pickle
-    else:
-        import dill as pickle
-    pickle.dump(mdict,open(fname,'wb'),-1)
-
 def load_pickle(dr,date=False,variable_names={}):
     """
     Load variables in pickle to global workspace using keys as variable names.
@@ -148,43 +138,6 @@ def load_pickle(dr,date=False,variable_names={}):
         for key in inData.keys():
             backglobals[key] = inData[key]
         return inData.keys()
-    
-def save_vars(variables,fname,prefix=''):
-    """
-    2014-02-21
-        Pickle given variables.
-        variables: list of variable names
-        fname (str) : full name including suffix
-
-    """
-    import inspect,pickle,warnings
-    import scipy.io as sio
-    import numpy as np
-
-    frame = inspect.currentframe()
-    backglobals = frame.f_back.f_globals
-    locals = frame.f_locals
-    d = {}
-
-    for key in variables:
-        if key not in backglobals.keys():
-            warnings.warn('%s not in workspace.' % s)
-        else:
-            d[key] = backglobals[key]
-    pickle.dump(d,open(prefix+fname,'wb'),-1)
-
-    #mdict = {}
-    #frame = inspect.currentframe()
-    #backglobals = frame.f_back.f_globals
-    #for key in backglobals.keys():
-    #    t = type(backglobals[key])
-    #    if (not str.isupper(key) and not str.istitle(key)) and \
-    #        (t==np.ndarray or t==float or t==int):
-    #        exec('mdict['+'\''+key+'\'] = backglobals[\''+key+'\']')
-    #        print key
-    #sio.savemat(fname,mdict)
-
-    return
 
 def load_mat_file(dir,squeeze_me=True,variable_names={},disp=True):
     """
@@ -207,35 +160,3 @@ def load_mat_file(dir,squeeze_me=True,variable_names={},disp=True):
         print inData.keys()
     return
 
-def save_vars_v0(file):
-    import shelve
-    import inspect
-
-    frame = inspect.currentframe()
-    keys = []
-    my_shelf = shelve.open(file,'n')
-    for key in (frame.f_back.f_locals).keys():
-        if not key.startswith('_'):
-            try:
-                if (not inspect.ismodule((frame.f_back.f_locals)[key])) and (not inspect.isroutine((frame.f_back.f_locals)[key])) and (not inspect.isclass((frame.f_back.f_locals)[key])):
-                    print(type(key))
-                    my_shelf[key] = (frame.f_back.f_locals)[key]
-                    keys.append(key)
-            except:
-               print('ERROR shelving: {0}'.format(key))
-    del frame
-    print(len(keys))
-    my_shelf.close()
-
-def load_vars_v0(file):
-    import shelve
-    import inspect
-
-    frame = inspect.currentframe()
-    my_shelf = shelve.open(file)
-    print("Loading...")
-    for key in my_shelf:
-        frame.f_back.f_globals[key] = my_shelf[key]
-        print(key)
-    del frame
-    my_shelf.close()
